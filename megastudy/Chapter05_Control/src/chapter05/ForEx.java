19a3334d4d6d9346948af726a87e90ef5086f5b8
Kyle <propheta@yonsei.ac.kr>
2026-07-12 23:05:14 +0900

    제어문 - 반복문 - for문

---
+++ megastudy/Chapter05_Control/src/chapter05/ForEx.java
@@ -0,0 +1,21 @@
+package chapter05;
+
+/**
+ * <h1>for문</h1>
+ * <p>
+ * for (초기화식; 조건식; 증감식) {
+ *     실행문;    // 조건식이 true인 경우에만 실행
+ * }
+ * </p>
+ *
+ * @author Kyle
+ * @since 2026. 07. 12.
+ */
+public class ForEx {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
